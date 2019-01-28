fa4b810ad3fa2d373928db463e808b9d2b3c5216
kdy0311 <ehdud_0311@naver.com>
2019-01-28 13:20:48 +0900

    19.01.28

--- app/src/main/java/com/honam/kyyeto/hfarmers/DetailGoodsActivity.java
+++ app/src/main/java/com/honam/kyyeto/hfarmers/DetailGoodsActivity.java
@@ -1,16 +1,24 @@
 package com.honam.kyyeto.hfarmers;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.view.View.TEXT_ALIGNMENT_CENTER;
 
 
 public class DetailGoodsActivity extends AppCompatActivity {
@@ -20,7 +28,6 @@ public class DetailGoodsActivity extends AppCompatActivity {
     private Button btnMinus;
     private Button btnKind;
     private TextView tvTitle;
-    private int count = 1;
     private Choice choice = new Choice();
     private Button[][] btns = new Button[3][2];
     private TextView[] views = new TextView[3];
@@ -46,7 +53,25 @@ public class DetailGoodsActivity extends AppCompatActivity {
         btnPurchase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(DetailGoodsActivity.this, views[0].getText() + " : " + views[1].getText() + " : " + views[2].getText() , Toast.LENGTH_SHORT).show();
+                new AlertDialog.Builder(DetailGoodsActivity.this)
+                        .setTitle("")
+                        .setMessage("해당 상품은 최소 1개 이상" +
+                                "주문하셔야 합니다.")
+                        .setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).show();
+                int count;
+                for(int i = 0; i > 3; i++){
+                    count =Integer.parseInt(String.valueOf(views[i].getText()));
+                    if(count < 1){
+                        Toast.makeText(DetailGoodsActivity.this, views[0].getText() , Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                //Toast.makeText(DetailGoodsActivity.this, views[0].getText() + " : " + views[1].getText() + " : " + views[2].getText() , Toast.LENGTH_SHORT).show();
                 //Intent intent = new Intent(DetailGoodsActivity.this, OrderActivity.class);
                 //startActivity(intent);
             }
@@ -61,13 +86,14 @@ public class DetailGoodsActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which)
             {
-                LinearLayout con = (LinearLayout)findViewById(R.id.con);
+                LinearLayout con = (LinearLayout) findViewById(R.id.con);
 
                 String text = "";
                 switch (which){
                     case 0 : text = "2kg";
                         if(choice.one == true) {
                             Toast.makeText(getApplicationContext(), "이미 추가되어 있습니다.", Toast.LENGTH_SHORT).show();
+                            dialog.cancel();
                             break;
                         }
                         choice.one = true;
@@ -76,6 +102,7 @@ public class DetailGoodsActivity extends AppCompatActivity {
                     case 1 : text = "1kg";
                         if(choice.two == true) {
                             Toast.makeText(getApplicationContext(), "이미 추가되어 있습니다.", Toast.LENGTH_SHORT).show();
+                            dialog.cancel();
                             break;
                         }
                         choice.two = true;
@@ -84,6 +111,7 @@ public class DetailGoodsActivity extends AppCompatActivity {
                     case 2 : text = "500g";
                         if(choice.three == true) {
                             Toast.makeText(getApplicationContext(), "이미 추가되어 있습니다.", Toast.LENGTH_SHORT).show();
+                            dialog.cancel();
                             break;
                         }
                         choice.three = true;
@@ -101,30 +129,77 @@ public class DetailGoodsActivity extends AppCompatActivity {
         });
 
     }
-
+    public int cal(int a){
+        float mScale = getResources().getDisplayMetrics().density;
+        final int calHeight = (int)(a*mScale);
+        return calHeight;
+    }
+    @SuppressLint("ResourceType")
     public void addView(String text, LinearLayout con, int num){
+        //RelativeLayout rl = new RelativeLayout(getApplicationContext());
         final TextView tv1 = new TextView(getApplicationContext());
+        tv1.setId(1);
         tv1.setText(text);
+        RelativeLayout.LayoutParams topTvParams = new RelativeLayout.LayoutParams
+                (ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+        tv1.setLayoutParams(topTvParams);
         tv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "gggggdfasdasdasd", Toast.LENGTH_SHORT).show();
             }
         });
+
+        final EditText et1 = new EditText(getApplicationContext());
+        et1.setId(2);
+        et1.setText("1");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            et1.setTextAlignment(TEXT_ALIGNMENT_CENTER);
+        }
+        RelativeLayout.LayoutParams middleEtParams = new RelativeLayout.LayoutParams
+                (180,150);
+        middleEtParams.addRule(RelativeLayout.BELOW, 1);
+        et1.setLayoutParams(middleEtParams);
+
+        btns[num][0].setId(3);
+        btns[num][0].setText("+");
+        RelativeLayout.LayoutParams plusBtnParams = new RelativeLayout.LayoutParams
+                (150,150);
+        plusBtnParams.addRule(RelativeLayout.ALIGN_TOP, 2);
+        plusBtnParams.addRule(RelativeLayout.RIGHT_OF, 2);
+        btns[num][0].setLayoutParams(plusBtnParams);
+
+        btns[num][1].setId(4);
+        btns[num][1].setText("-");
+        RelativeLayout.LayoutParams minusBtnParams = new RelativeLayout.LayoutParams
+                (150,150);
+        minusBtnParams.addRule(RelativeLayout.ALIGN_TOP, 2);
+        minusBtnParams.addRule(RelativeLayout.RIGHT_OF, 3);
+        btns[num][1].setLayoutParams(minusBtnParams);
+
         btns[num][0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv1.setText("가격이 올라감");
+                int count = (int) Integer.parseInt(String.valueOf(et1.getText()));
+                count++;
+                et1.setText(""+count);
             }
         });
         btns[num][1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv1.setText("가격이 내려감");
+                int count = (int) Integer.parseInt(String.valueOf(et1.getText()));
+                if(count > 1){
+                    count--;
+                }
+
+                et1.setText(""+count);
             }
         });
-        views[num] = tv1;
+        views[num] = et1;
         con.addView(tv1);
+        con.addView(et1);
         con.addView(btns[num][0]);
         con.addView(btns[num][1]);
     }
